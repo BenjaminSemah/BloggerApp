a25d516da8a15406cacf4e4bd40ffa4015f56e4f
Benjamin Semah <benjaminsemah@gmail.com>
2022-05-04 22:40:56 +0000

    Fix linters errors

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -1,39 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
+RSpec.describe 'All Posts', type: :request do
   describe 'GET posts#index' do
     before :each do
       get '/users/29/posts'
     end
 
-    it "checks if response status was correct" do
+    it 'checks if response status was correct' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "checks if correct template was rendered" do
+    it 'checks if correct template was rendered' do
       expect(response).to render_template('index')
     end
 
-    it "checks if response includes placeholder" do
-      expect(response.body).to include("Display ALL POSTS for a user")
+    it 'checks if response includes placeholder' do
+      expect(response.body).to include('Display ALL POSTS for a user')
     end
   end
+end
 
+RSpec.describe 'Singe Post', type: :request do
   describe 'GET posts#show' do
     before :each do
       get '/users/29/posts/29'
     end
 
-    it "checks if response status was correct" do
+    it 'checks if response status was correct' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "checks if correct template was rendered" do
+    it 'checks if correct template was rendered' do
       expect(response).to render_template('show')
     end
 
-    it "checks if response includes placholder" do
-      expect(response.body).to include("Display SINGLE POST for a user")
+    it 'checks if response includes placholder' do
+      expect(response.body).to include('Display SINGLE POST for a user')
     end
   end
 end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,39 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET users#index" do
+RSpec.describe 'All Users', type: :request do
+  describe 'GET users#index' do
     before :each do
       get users_path
     end
 
-    it "checks if response status was correct" do
+    it 'checks if response status was correct' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "checks if a correct template was rendered" do
+    it 'checks if a correct template was rendered' do
       expect(response).to render_template('index')
     end
 
-    it "checks if response body includes placeholder" do
-      expect(response.body).to include("This page will display ALL USER")
+    it 'checks if response body includes placeholder' do
+      expect(response.body).to include('This page will display ALL USER')
     end
   end
+end
 
-  describe "GET users#show" do
+RSpec.describe 'Single Users', type: :request do
+  describe 'GET users#show' do
     before :each do
       get user_path(id: 29)
     end
 
-    it "checks if response status was correct" do
+    it 'checks if response status was correct' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "checks if a correct template was rendered" do
+    it 'checks if a correct template was rendered' do
       expect(response).to render_template('show')
     end
 
-    it "checks if response body includes placeholder" do
-      expect(response.body).to include("This page will display a SINGLE USER")
+    it 'checks if response body includes placeholder' do
+      expect(response.body).to include('This page will display a SINGLE USER')
     end
   end
 end
