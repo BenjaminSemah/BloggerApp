e2dc21735ea7a9836f32a65fd6a02bf406256bcf
Benjamin Semah <benjaminsemah@gmail.com>
2022-05-04 22:12:33 +0000

    Add tests for GET users#show

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -18,4 +18,22 @@ RSpec.describe "Users", type: :request do
       expect(response.body).to include("This page will display ALL USER")
     end
   end
+
+  describe "GET users#show" do
+    before :each do
+      get user_path(id: 29)
+    end
+
+    it "checks if response status was correct" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "checks if a correct template was rendered" do
+      expect(response).to render_template('show')
+    end
+
+    it "checks if response body includes placeholder" do
+      expect(response.body).to include("This page will display a SINGLE USER")
+    end
+  end
 end
