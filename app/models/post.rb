54506bedd79145b16926a07458f7a6b4b02623cd
Benjamin Semah <benjaminsemah@gmail.com>
2022-05-09 18:32:47 +0000

    Create method for recents comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,16 +3,13 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  # def count_posts
-  #   counter = Post.where(author_id: id).count
-  #   user = user.find_by(id)
-  #   user.update(posts_counter: counter)
-  # end
-
-
   def update_posts_counter
     counter = Post.where(author_id: self.author_id).count
     user = User.find_by(id: self.author_id)
     user.update(posts_counter: counter)
+  end
+
+  def recent_comments
+    Comment.where(post_id: self.id).limit(5).order(created_at: :desc)
   end
 end
