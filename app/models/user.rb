0813acf1e670d612dd9c76cbce85d3875bc90711
Benjamin Semah <benjaminsemah@gmail.com>
2022-05-09 17:26:54 +0000

    Create method for User's recent posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
+
+  def recent_posts
+    Post.where(author_id: self.id).limit(3).order(created_at: :desc)
+  end
 end
+
+
