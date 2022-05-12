0fe1e8c18390200b886a6ab0928a4367942725cd
Benjamin Semah <benjaminsemah@gmail.com>
2022-05-12 08:25:47 +0000

    Add view for index page of posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,18 @@
 # app/view/posts
 class PostsController < ApplicationController
+  # def index
+  #   @all_posts = User.find_by(id: params[:user_id]).posts
+  #   @user = User.find_by(id: params[:user_id])
+  #   @comments = Comment.where(post_id: @post.id)
+  # end
+
   def index
-    render 'index'
+    @user = User.find_by(id: params[:user_id])
+    @all_posts = @user.posts
   end
 
   def show
-    render 'show'
+    @post = User.find_by(id: params[:user_id]).posts.find_by(id: params[:id])
+    @user = User.find_by(id: @post.author.id)
   end
 end
