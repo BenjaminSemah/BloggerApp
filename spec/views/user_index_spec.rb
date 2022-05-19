604596a62addea6fb068922a9016217d17340205
Benjamin Semah <benjaminsemah@gmail.com>
2022-05-19 12:25:16 +0000

    Add integration test for users#show

--- spec/views/user_index_spec.rb
+++ spec/views/user_index_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe 'user_index_path', type: :feature do
       @first_user = User.create(name: 'Benjamin', photo: 'url', bio: 'Developer from Ghana',
                                 posts_counter: 0, email: 'benjamin@mail.com', password: 'password', password_confirmation: 'password')
       @first_user = User.create(name: 'Edmond', photo: 'url', bio: 'Designer from Ghana',
-                                posts_counter: 0, email: 'edmond@mail.com', password: 'password', password_confirmation: 'password')
+                                posts_counter: 2, email: 'edmond@mail.com', password: 'password', password_confirmation: 'password')
       @second_user = User.create(name: 'Theo', photo: 'url', bio: 'Developer from Ghana',
-                                posts_counter: 0, email: 'theo@mail.com', password: 'password', password_confirmation: 'password')
+                                posts_counter: 4, email: 'theo@mail.com', password: 'password', password_confirmation: 'password')
 
       visit('users/sign_in')
       fill_in 'Email', with: 'benjamin@mail.com'
@@ -18,20 +18,23 @@ RSpec.describe 'user_index_path', type: :feature do
       click_link 'All Users'
     end
 
-    it 'Check for user profile picture' do
-      all('img').each do |i|
-        expect(i[:src]).to eq('url')
-      end
-    end
 
-    it 'displays usernames' do
+    it 'displays username of all other users' do
       expect(page).to have_content('Benjamin')
       expect(page).to have_content('Edmond')
       expect(page).to have_content('Theo')
     end
 
-    it 'displays number of posts by users' do
+    it 'displys profile picture of each user' do
+      all('img').each do |i|
+        expect(i[:src]).to eq('url')
+      end
+    end
+
+    it 'displays number of posts each user has' do
       expect(page).to have_content(0)
+      expect(page).to have_content(2)
+      expect(page).to have_content(4)
     end
 
     it "redirects to user's show page" do
--- spec/views/user_show_spec.rb
+++ spec/views/user_show_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'user_show_path', type: :feature do
   describe 'User show page' do
     before(:each) do
-      @first_user = User.create(name: 'Victor', photo: 'link', bio: 'bio', posts_counter: 0,
+      @first_user = User.create(name: 'Victor', photo: 'link', bio: 'bio', posts_counter: 4,
                                 email: 'victor@gmail', password: '123456', password_confirmation: '123456')
       @second_user = User.create(name: 'Bad', photo: 'link', bio: 'bio', posts_counter: 0,
                                  email: 'devi@gmail', password: '123456', password_confirmation: '123456')
@@ -15,29 +15,12 @@ RSpec.describe 'user_show_path', type: :feature do
       fill_in 'Password', with: '123456'
       click_button 'Log in'
 
-      @first_post = Post.create(user: @first_user, title: 'Finding Dora', text: 'This is my first post')
-      @second_post = Post.create(user: @first_user, title: 'Finding Dora', text: 'This is my second post')
-      @third_post = Post.create(user: @first_user, title: 'Finding Dora', text: 'This is my third post')
+      @first_post = Post.create(user: @first_user, title: 'Number One', text: 'This is my first post')
+      @second_post = Post.create(user: @first_user, title: 'Number Two', text: 'This is my second post')
+      @third_post = Post.create(user: @first_user, title: 'Number Three', text: 'This is my third post')
+      @fourth_post = Post.create(user: @first_user, title: 'Number Four', text: 'This is my fourth post')
 
       visit user_path(@first_user.id)
-    end
-
-    it 'displays username' do
-      expect(page).to have_content('Victor')
-    end
-
-    it 'displays number of posts by user' do
-      expect(page).to have_content(3)
-    end
-
-    it 'displays posts' do
-      expect(page).to have_content('This is my first post')
-      expect(page).to have_content('This is my second post')
-      expect(page).to have_content('This is my third post')
-    end
-
-    it 'displays user bio' do
-      expect(page).to have_content('bio')
     end
 
     it 'Check for user profile picture' do
@@ -45,12 +28,37 @@ RSpec.describe 'user_show_path', type: :feature do
         expect(i[:src]).to eq('link')
       end
     end
-    
-    it 'displays button to see all posts' do
+
+    it 'displays username' do
+      expect(page).to have_content('Victor')
+    end
+
+    it 'displays number of posts by user' do
+      expect(page).to have_content(4)
+    end
+
+    it 'displays user bio' do
+      expect(page).to have_content('bio')
+    end
+
+    # it 'displays posts' do
+    #   expect(page).to have_content('Number Two')
+    # end
+
+
+    it 'displys profile picture of each user' do
+      @first_user.recent_posts.each do |post|
+        expect(post).to eq('This is my fourth post')
+        expect(post).to eq('This is my third post')
+        expect(post).to eq('This is my second post')
+      end
+    end
+
+    it 'displays button to see all posts by user' do
       expect(page).to have_button('See all posts')
     end
 
-    it 'displays  post onclick and redirects to posts page' do
+    it 'displays all post by user when all post button is clicked' do
       click_link 'See all posts'
       expect(page).to have_current_path user_posts_path(@first_user)
     end
