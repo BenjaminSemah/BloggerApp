00db0497a9a6dfa9c5fc78a21a1c7816b2dca6f6
Benjamin Semah <benjaminsemah@gmail.com>
2022-05-21 09:22:07 +0000

    update controller for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-first_user = User.create(email: 'test@email.com', password: 'password', name: 'Tom', photo: 'https://image.shutterstock.com/image-vector/man-character-face-avatar-glasses-600w-542759665.jpg', bio: 'Teacher from Mexico.', posts_counter: 0)
-second_user = User.create(email: 'test1@email.com', password: 'password', name: 'Lilly', photo: 'https://image.shutterstock.com/image-vector/female-face-avatar-on-white-600w-562359607.jpg', bio: 'Teacher from Poland.', posts_counter: 0)
-
-first_post = Post.create(user_id: first_user, title: 'Hello', text: 'This is first post', comments_counter: 0, likes_counter: 0)
-second_post = Post.create(user_id: first_user, title: 'Hello', text: 'This issecond post', comments_counter: 0, likes_counter: 0)
-third_post = Post.create(user_id: second_user, title: 'Hello', text: 'This isthird post', comments_counter: 0, likes_counter: 0)
-fourth_post = Post.create(user_id: second_user, title: 'Hello', text: 'This is fourth post', comments_counter: 0, likes_counter: 0)
-
-Comment.create(post_id: first_post, user_id: second_user, text: 'Hi Tom!' )
-Comment.create(post_id: first_post, user_id: first_user, text: 'Hi Lilly!' )
-Comment.create(post_id: second_post, user_id: second_user, text: 'Hi , this is my first comment' )
-Comment.create(post_id: second_post, user_id: second_user, text: 'Hi , this is my second comment' )
-
-Like.create(post_id: first_post, user_id: second_user)
-Like.create(post_id: second_post, user_id: first_user)
-
-puts 'Seed data loaded successfully'
